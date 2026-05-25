01f2dc2a90c1c1cb52477093e427d6e72c6c17c7
framnz85 <clavmall.85@gmail.com>
2026-05-26 06:41:48 +0800

    commit 5-26-26 6:00 am

--- server.js
+++ server.js
@@ -13,7 +13,37 @@ if (process.env.NODE_ENV !== "production") {
 }
 
 app.use(express.json({ limit: "2mb" }));
-app.use(cors());
+
+const allowedOrigins = [
+  "https://cosmic.clavstore.com",
+  "https://store.clavstore.com",
+  "https://clavstore.com",
+  "https://www.clavstore.com",
+  "https://learnclavstore.com",
+  "https://www.learnclavstore.com",
+  "http://localhost:3005",
+  "http://localhost:3004",
+  "capacitor://localhost",
+  "ionic://localhost",
+  "http://localhost",
+];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) {
+        return callback(null, true);
+      }
+      if (allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
+    methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
+    credentials: true,
+  }),
+);
 
 readdirSync("./routes").map((file) =>
   app.use("/" + process.env.API_ROUTES, require("./routes/" + file)),
